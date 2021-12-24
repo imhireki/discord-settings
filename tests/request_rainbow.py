cc0eb806f4cf27f844ae4af7b4c92c8d031b5815
imHireki <imhireki@gmail.com>
2021-12-24 08:02:20 -0300

    refactor: oop the code

--- tests/request_rainbow.py
+++ tests/request_rainbow.py
@@ -1,30 +1,54 @@
+#! /usr/bin/python3
+
 import requests
 from time import sleep
 
 
+class RainbowPuke:
+    def __init__(self, JWT, urls, timeouts):
+        self.timeouts = timeouts
+        self.urls = urls
+        self.headers = JWT
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @headers.setter
+    def headers(self, value):
+        self._headers = {'Content-Type': 'application/json',
+                         'authorization': value,
+                         'Content-Length': '0'}
+
+    def perform_color_change(self):
+        prev_url = None
+
+        while True:
+            for url in self.urls:
+                self.add_color(url)
+
+                if prev_url:
+                    self.rem_color(prev_url)
+
+                prev_url = url
+
+    def add_color(self, url):
+        requests.put(url,
+                     headers=self.headers)
+
+        sleep(self.timeouts['PUT']) # timeout pos PUT
+
+    def rem_color(self, url):
+        requests.delete(url,
+                        headers=self.headers)
+
+        sleep(self.timeouts['DELETE']) # timeout pos DELETE
+
+
 if __name__ == '__main__':
-    urls = [] 
+    JWT = ''
+    urls = []
+    timeouts = {}
 
-    JWT = '' 
-
-    request_headers = {
-        'Content-Type': 'application/json',
-        'authorization': JWT,
-        'Content-Length': '0',
-        } 
-
-    previous = None
-    while True:
-        for url in urls:
-            request = requests.put(
-                url,
-                headers=request_headers
-                ) 
-            sleep(0.5)
-            if previous:
-                request2 = requests.delete(
-                    previous, headers=request_headers
-                    )
-            previous = url
-            sleep(1)
-
+    rp = RainbowPuke(JWT, urls, timeouts)
+    rp.perform_color_change()
