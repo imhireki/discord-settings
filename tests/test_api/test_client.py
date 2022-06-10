8499bf0f1e1ea4f53912bf06679cf563da1b29e5
imHireki <imhireki@gmail.com>
2022-06-10 19:41:56 -0300

    [test]: add get and patch tests for the client v9

---
+++ tests/test_api/test_client.py
@@ -0,0 +1,46 @@
+import pytest
+from api import client
+
+
+def dict_items_in_dict(dict_a: dict, dict_b: dict):
+    match_items = dict([
+        a
+        for a in dict_a.items()
+        for b in dict_b.items()
+        if a == b
+    ])
+    return True if match_items == dict_a else False
+
+
+def test_client_get_request(requests_mock, auth_token):
+    request_client = client.RequestClientV9(auth_token)
+
+    _get_request = request_client.request_manager._requests['GET']
+    get_headers = _get_request.request_method.headers
+
+    text_data = '{"test": 123}'
+    requests_mock.get(request_client.ENDPOINT, text=text_data)
+
+    response = request_client.get()
+
+    assert response.status_code == 200
+    assert response.url == request_client.ENDPOINT
+    assert response.text == text_data
+    assert dict_items_in_dict(get_headers, response.request.headers)
+
+
+def test_client_patch_request(requests_mock, auth_token):
+    request_client = client.RequestClientV9(auth_token)
+
+    _patch_request = request_client.request_manager._requests['PATCH']
+    patch_headers = _patch_request.request_method.headers
+
+    data = {'data': 123}
+    requests_mock.patch(request_client.ENDPOINT, json=data)
+
+    response = request_client.patch(data)
+
+    assert response.status_code == 200
+    assert response.url == request_client.ENDPOINT
+    assert eval(response.text) == data
+    assert dict_items_in_dict(patch_headers, response.request.headers)
