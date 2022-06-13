a426dda45bb49438a742868894d63c943fd4d2db
imHireki <imhireki@gmail.com>
2022-06-13 10:48:08 -0300

    [fix]: replace eval for json.loads in the tests

--- tests/test_settings/test_settings.py
+++ tests/test_settings/test_settings.py
@@ -1,15 +1,16 @@
 from settings import settings
 from copy import deepcopy
+import json
 
 
 def test_local_settings(mocker):
-    remote_settings = {"status": "dnd", "custom_status": {"text": "xD"}}
+    remote_settings = '{"status": "dnd", "custom_status": {"text": "xD"}}'
     new_settings = {"status": "online", "a": "1"}
 
-    remote_settings_updated = deepcopy(remote_settings)
+    remote_settings_updated = deepcopy(json.loads(remote_settings))
     remote_settings_updated.update(new_settings)
 
-    response_mock = mocker.Mock(text=str(remote_settings))
+    response_mock = mocker.Mock(text=remote_settings)
     request_client_mock = mocker.Mock(get=mocker.Mock(return_value=response_mock))
 
     local_settings = settings.LocalSettings(request_client_mock)
