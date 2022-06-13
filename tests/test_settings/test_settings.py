66dec420045efed6f6853e76398e6999a6f10259
imHireki <imhireki@gmail.com>
2022-06-13 10:23:49 -0300

    [test]: add tests to the settings module

---
+++ tests/test_settings/test_settings.py
@@ -0,0 +1,18 @@
+from settings import settings
+from copy import deepcopy
+
+
+def test_local_settings(mocker):
+    remote_settings = {"status": "dnd", "custom_status": {"text": "xD"}}
+    new_settings = {"status": "online", "a": "1"}
+
+    remote_settings_updated = deepcopy(remote_settings)
+    remote_settings_updated.update(new_settings)
+
+    response_mock = mocker.Mock(text=str(remote_settings))
+    request_client_mock = mocker.Mock(get=mocker.Mock(return_value=response_mock))
+
+    local_settings = settings.LocalSettings(request_client_mock)
+    local_settings.update(new_settings)
+
+    assert local_settings._settings == remote_settings_updated
