9ee3d9c0da940b8f25b644b05a37dc0b9f898bd0
imHireki <imhireki@gmail.com>
2022-06-14 16:27:24 -0300

    [test]: add test to the SettingsBuffer

--- tests/test_settings/test_settings.py
+++ tests/test_settings/test_settings.py
@@ -1,6 +1,10 @@
-from settings import settings
 from copy import deepcopy
 import json
+
+import pytest
+
+from settings import settings
+from iterables import iterable
 
 
 def test_local_settings(mocker):
@@ -17,3 +21,37 @@ def test_local_settings(mocker):
     local_settings.update(new_settings)
 
     assert local_settings._settings == remote_settings_updated
+
+
+def test_settings_buffer(mocker):
+    class SettingA(iterable.ISettingIterable):
+        def __init__(self) -> None:
+            super().__init__(["1", "2", "3"])
+
+        def get_request_data(self, value: str) -> dict[str, str]:
+            return {"setting": {"a": value}}
+
+
+    class SettingB(iterable.ISettingIterable):
+        def __init__(self) -> None:
+            super().__init__(["x", "y", "z"])
+
+        def get_request_data(self, value: str) -> dict[str, str]:
+            return {"setting": {"b": value}}
+
+    setting_a = SettingA()
+    setting_a_iter = iter(setting_a)
+
+    setting_b = SettingB()
+    setting_b_iter = iter(setting_b)
+
+    settings_buffer = settings.SettingsBuffer(
+        iterables=[setting_a, setting_b],
+        iterators=[setting_a_iter, setting_b_iter]
+    )
+
+    for item_a, item_b in zip(setting_a._items, setting_b._items):
+
+        assert settings_buffer.get_updated_buffer() == {
+            "setting": {"a": item_a, "b": item_b}
+        }
