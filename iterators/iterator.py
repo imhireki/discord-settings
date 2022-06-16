510292eb1a22326f7b1928f4610808d6c5014a41
imHireki <imhireki@gmail.com>
2022-06-16 14:41:10 -0300

    [feat]: add IUnlimitedItems interface

--- iterators/iterator.py
+++ iterators/iterator.py
@@ -19,3 +19,20 @@ class ISettingIterator(ABC):
     @abstractmethod
     def reset_iterable_index(self): pass
 
+
+class IUnlimitedItems(ISettingIterator):
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.max_iterable_index = len(self.iterable) - 1
+
+    def indexes_in_iterable_range(self, index, max_index):
+        return True if 0 <= index <= max_index else False
+
+    def __next__(self):
+        self.set_next_iterable_index()
+
+        if not self.indexes_in_iterable_range(self.iterable_index,
+                                              self.max_iterable_index):
+            self.reset_iterable_index()
+
+        return self.get_iterable_item()
