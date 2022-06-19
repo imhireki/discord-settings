b8fbd44eb8bd84061d18d4276ea9a4e82628269e
imHireki <imhireki@gmail.com>
2022-06-19 14:58:52 -0300

    [test]: add Decrease iterator

--- tests/test_iterators/test_iterator.py
+++ tests/test_iterators/test_iterator.py
@@ -9,6 +9,13 @@ def test_increase():
 
     assert next(increase_iterator) == iterable[0]
     assert next(increase_iterator) == iterable[1]
-
     assert next(increase_iterator) == iterable[0]
-    # StopIteration not raised !
+
+
+def test_decrease():
+    iterable = ["abc", "xyz"]
+    decrease_iterator = iterator.Decrease(iterable)
+
+    assert next(decrease_iterator) == iterable[1]
+    assert next(decrease_iterator) == iterable[0]
+    assert next(decrease_iterator) == iterable[1]
