c03a53f029ccde7e73d93b2740b5f2adf8496f08
imHireki <imhireki@gmail.com>
2022-06-22 15:57:51 -0300

    [test]: add test_items_before_next_index

--- tests/test_iterators/test_iterator.py
+++ tests/test_iterators/test_iterator.py
@@ -11,7 +11,6 @@ def test_increase():
     assert next(increase_iterator) == iterable[1]
     assert next(increase_iterator) == iterable[0]
 
-
 def test_decrease():
     iterable = ["abc", "xyz"]
     decrease_iterator = iterator.Decrease(iterable)
@@ -20,13 +19,22 @@ def test_decrease():
     assert next(decrease_iterator) == iterable[0]
     assert next(decrease_iterator) == iterable[1]
 
-
 def test_single_item():
-    iterable = "abc"
+    iterable = ["abc"]
     abc_iterator = iter(iterable)
 
     single_item_iterator = iterator.SingleItem(abc_iterator)
 
-    assert next(single_item_iterator) == iterable[0]
-    assert next(single_item_iterator) == iterable[1]
-    assert next(single_item_iterator) == iterable[2]
+    assert next(single_item_iterator) == iterable[0][0]
+    assert next(single_item_iterator) == iterable[0][1]
+    assert next(single_item_iterator) == iterable[0][2]
+
+def test_items_before_next_index():
+    iterable = ["xyz"]
+    xyz_iterator = iter(iterable)
+
+    items_before_next_index_iterator = iterator.ItemsBeforeNextIndex(xyz_iterator)
+
+    assert next(items_before_next_index_iterator) == iterable[0][:1]
+    assert next(items_before_next_index_iterator) == iterable[0][:2]
+    assert next(items_before_next_index_iterator) == iterable[0][:3]
