ef00b5e31f5ac2c25906ef2be91a0aa1c6189252
imHireki <imhireki@gmail.com>
2022-06-22 15:30:17 -0300

    [test]: add test_single_item

--- tests/test_iterators/test_iterator.py
+++ tests/test_iterators/test_iterator.py
@@ -19,3 +19,14 @@ def test_decrease():
     assert next(decrease_iterator) == iterable[1]
     assert next(decrease_iterator) == iterable[0]
     assert next(decrease_iterator) == iterable[1]
+
+
+def test_single_item():
+    iterable = "abc"
+    abc_iterator = iter(iterable)
+
+    single_item_iterator = iterator.SingleItem(abc_iterator)
+
+    assert next(single_item_iterator) == iterable[0]
+    assert next(single_item_iterator) == iterable[1]
+    assert next(single_item_iterator) == iterable[2]
