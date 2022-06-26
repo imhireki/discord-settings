c6628a665cbbfb45fdb4716ceb44ada77809b28e
imHireki <imhireki@gmail.com>
2022-06-26 08:59:22 -0300

    [test]: improve test on iterables

--- tests/test_iterables/test_iterable.py
+++ tests/test_iterables/test_iterable.py
@@ -2,18 +2,23 @@ from iterables import iterable
 import pytest
 
 
-emojis = ["💫", "🌟"]
-text_list = ["x", "y", "z"]
+emojis = ['💫', '✨', '⭐']
+text_list = ['x', 'y', 'z']
 
 @pytest.mark.parametrize('setting', [
     iterable.EmojiName(emojis),
     iterable.Text(text_list),
     iterable.Status()
 ])
-def test_default_iterator(mocker, setting):
-    iterator_mock = mocker.patch('iterators.iterator.Unlimited')
+def test_iterators(mocker, setting):
+    increase_iterator_mock = mocker.patch('iterators.iterator.Increase')
+    decrease_iterator_mock = mocker.patch('iterators.iterator.Decrease')
+
     iter(setting)
-    iterator_mock.assert_called_with(setting._items)
+    setting.decrease()
+
+    increase_iterator_mock.assert_called_with(setting)
+    decrease_iterator_mock.assert_called_with(setting)
 
 def test_emoji_name_setting():
     emoji_name_iterable = iterable.EmojiName(emojis)
@@ -22,21 +27,16 @@ def test_emoji_name_setting():
         expected_data = {"custom_status": {"emoji_name": emoji}}
         assert emoji_name_iterable.get_request_data(emoji) == expected_data
 
-def test_text_setting(mocker):
+def test_text_setting():
     text_iterable = iterable.Text(text_list)
-    data_before_index_mock = mocker.patch('iterators.iterator.MultipleBeforeIndex')
-
-    text_iterable.iter_data_before_index()
 
     for text in text_list:
         expected_data = {"custom_status": {"text": text}}
         assert text_iterable.get_request_data(text) == expected_data
 
-    data_before_index_mock.assert_called_with(text_list)
-
 def test_status_setting():
     status_iterable = iterable.Status()
 
-    for status in status_iterable._items:
+    for status in status_iterable.items:
         expected_data = {"status": status}
         assert status_iterable.get_request_data(status) == expected_data
