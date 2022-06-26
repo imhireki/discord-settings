76468c477274314d1a019b76e618bc2ce7c56465
imHireki <imhireki@gmail.com>
2022-06-26 15:07:48 -0300

    [test]: remake the settings mock and its assertion

--- tests/test_polls/test_poll.py
+++ tests/test_polls/test_poll.py
@@ -8,16 +8,22 @@ def test_poll(mocker, updated_buffer):
     def get_updated_buffer():
         return updated_buffer
 
-    def polling():
-        settings.state = buffer.get_updated_buffer()
-
     buffer = mocker.Mock(get_updated_buffer=get_updated_buffer)
-    settings = mocker.Mock(state={})
+
+    class LocalSettings:
+        _settings = {}
+
+        def update(self, settings):
+            self._settings.update(settings)
+
+    def polling():
+        settings.update(buffer.get_updated_buffer())
+
+    settings = LocalSettings()
 
     _poll = poll.Poll(settings, buffer)
     mocker.patch.object(_poll, 'polling', return_value=polling())
 
     _poll.polling()
 
-    assert settings.state == updated_buffer
-
+    assert settings._settings == updated_buffer
